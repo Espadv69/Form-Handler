112f36efd22553996addf864ddbd811cfaea5482
espadv69 <aitorjob69@gmail.com>
2025-01-22 12:10:55 +0100

    Add UserForm import and userData state management in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
-const App = () => {
+import { useState, useEffect } from 'react'
 
+import UserForm from './components/UserForm'
+
+const App = () => {
+  const [userData, setUserData] = useState(null)
 }
 
 export default App
