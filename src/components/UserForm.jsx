9b69121453ae265a2b93c769e14ff4ec60f01852
espadv69 <aitorjob69@gmail.com>
2025-01-22 12:02:37 +0100

    Implement form submission handling and reset inputs in UserForm component

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -3,4 +3,25 @@ import { useState } from 'react'
 const UserForm = ({ onSubmit }) => {
   const [user, setUser] = useState('') // Input
   const [email, setEmail] = useState('') // Input
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    onSubmit({ user, email }) // Send data
+    setUser('') // Reset input
+    setEmail('') // Reset input
+  }
+
+  return (
+    <form action="">
+      <div>
+        <label htmlFor="">
+          User:
+          <input type="text" />
+        </label>
+      </div>
+    </form>
+  )
 }
+
+export default UserForm
