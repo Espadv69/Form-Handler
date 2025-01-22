48c44014d815defeaa174dfe1d268123cecfe070
espadv69 <aitorjob69@gmail.com>
2025-01-22 12:12:26 +0100

    Add user submission handling to App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,18 @@ import UserForm from './components/UserForm'
 
 const App = () => {
   const [userData, setUserData] = useState(null)
+
+  const handleUserSubmit = (data) => {
+    setUserData(data)
+  }
+
+  return (
+    <main>
+      <h1 className="h1-title">Form Handling in React</h1>
+
+      <UserForm onSubmit={handleUserSubmit} />
+    </main>
+  )
 }
 
 export default App
