820bdca3eb32599834f443b9ef73e6ff40c4e335
espadv69 <aitorjob69@gmail.com>
2025-01-22 12:31:54 +0100

    Import global CSS in main.jsx for consistent styling

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDom from 'react-dom/client'
 
 import App from './App'
+import './css/global.css'
 
 const root = ReactDom.createRoot(document.getElementById('root'))
 
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
